a4da52a4380baef7594e1ed3e8337de2a7fbb94f
Sung Geun Yun <sy20cala@studserv.uni-leipzig.de>
2021-06-04 12:29:37 +0200

    finished item_category

--- src/entity/Categories.java
+++ src/entity/Categories.java
@@ -1,7 +1,10 @@
 package entity;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.EmptyStackException;
 import java.util.HashMap;
 import java.util.List;
@@ -128,6 +131,7 @@ public class Categories {
 		Stack<Node> dfsStack = new Stack<Node>();
 		dfsStack.push(currentNode.getCurrentNode());
 		dfs:while(currentNode.getCurrentNode() != null) {
+
 			if(currentNode.getCurrentNode().getNodeType() == Node.ELEMENT_NODE  && !"item".equals(currentNode.getCurrentNode().getNodeName())) {
 				//System.out.print(xt.getPrintOpeningNode(currentNode.getCurrentNode(), level));
 				Category category = new Category();	
@@ -164,6 +168,53 @@ public class Categories {
 				}
 			}
 			
+			// item_category
+			if(currentNode.getCurrentNode().getNodeType() == Node.ELEMENT_NODE  && "item".equals(currentNode.getCurrentNode().getNodeName())) {
+				Node categoryNode = currentNode.getCurrentNode().getParentNode();
+				String categoryName = xt.getFirstTextNodeValue(categoryNode);
+				int categoryId = this.getCategory_id(categoryName);
+
+				List<Node> itemNodeList = xt.getNodesByNameDFS(currentNode.getCurrentNode(), "item");
+				itemNodeList.forEach(itemNode -> {
+				//TODO
+					String item_id = xt.getTextContentOfLeafNode(itemNode);
+					
+					try {
+						JDBCTool.executeUpdateAutoCommitOn((con, st) -> {
+							String sql;
+							PreparedStatement ps;
+							sql = "INSERT INTO public.item_category("
+									+ "	item_id, category_id)"
+									+ "	VALUES (?, ?);";
+							ps = con.prepareStatement(sql);
+							ps.setString(1, item_id);
+							ps.setInt(2,categoryId);
+							ps.executeUpdate();
+							ps.close();	
+							
+						});
+					} catch (IllegalArgumentException e) {
+						ErrorLogger.write(location, item_id, ErrType.PROGRAM, "item_category" ,e, xt.getNodeContentDFS(currentNode.getCurrentNode()));
+					} catch (SQLException ex) {
+						if(ex.getMessage().contains("duplicate key value")) {
+							SQLKeyDuplicatedException e = new SQLKeyDuplicatedException();
+							e.setAttrName("item_id");
+							e.setItem_id(item_id);
+							e.setLocation(location);
+							e.setMessage("duplicate key value");
+							ErrorLogger.write(e, xt.getNodeContentDFS(currentNode.getCurrentNode()));
+						} else {
+							ErrorLogger.write(location, item_id, ErrType.SQL, "item_category", ex, xt.getNodeContentDFS(currentNode.getCurrentNode()));
+						}
+					} catch (Exception e) {
+						ErrorLogger.write(location, item_id, ErrType.PROGRAM, "item_category", e, xt.getNodeContentDFS(currentNode.getCurrentNode()));
+					}
+					
+					
+					
+				});
+			}
+			
 			if(currentNode.getCurrentNode().hasChildNodes()) {
 				dfsStack.push(currentNode.getCurrentNode());
 				level++;
@@ -194,6 +245,36 @@ public class Categories {
 	public void insertCategory_SubCategory() {
 		
 	}
+	
+	
+	@SuppressWarnings("finally")
+	public Integer getCategory_id(String categoryName) {
+		Category category = new Category();
+		String location = "getCategory_id";
+		try {
+			JDBCTool.executeUpdateAutoCommitOn((con, st) -> {
+				String sql;
+				PreparedStatement ps;
+				sql = "SELECT category_id FROM public.category\n"
+						+ "where name = (?)";
+				ps = con.prepareStatement(sql);
+				ps.setString(1, categoryName);
+				ResultSet rs = ps.executeQuery();
+				
+				while(rs.next()){
+					category.setCategory_id(rs.getInt(1));
+				}
+				ps.close();	
+			});
+		} catch (SQLException ex) {
+			ErrorLogger.write(location, categoryName, ErrType.SQL, "CategoryId", ex, categoryName+" is not in the table category.");
+		} catch (Exception e) {
+			ErrorLogger.write(location, categoryName, ErrType.PROGRAM, "CategoryId", e, categoryName+" is not in the table category.");
+		} finally {
+			return category.getCategory_id();
+		}
+    }
+	
 	
 	
 	public static Predicate<String> pred_categoryName = name -> name != null && name.length() > 0; //not null
--- src/entity/Category.java
+++ src/entity/Category.java
@@ -23,11 +23,9 @@ public class Category {
 	public int getCategory_id() {
 		return category_id;
 	}
-	/*
 	public void setCategory_id(int category_id) {
 		this.category_id = category_id;
 	}
-	 */
 	public String getCategoryName() {
 		return categoryName;
 	}
