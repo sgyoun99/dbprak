8700d9c3beddf7cb9f561f2c70c02c27aad88053
sgyoun99 <sgyoun99@gmail.com>
2021-06-18 10:43:05 +0200

    updated 4.sql

--- Testat2/4.sql
+++ Testat2/4.sql
@@ -1,12 +1,12 @@
 WITH
 item_id_count AS 
 	(SELECT item_id, COUNT(*) as item_id_count FROM item_shop GROUP BY item_id),
-in_many_shops AS 
+many_offers AS 
 	(SELECT item_id FROM item_id_count WHERE item_id_count >= 2),
-price_table AS --for the items which are offered at least by more than two shops.
+price_table AS --for the items which has more than two offers.
 	(SELECT * FROM item_shop
 	WHERE availability='true' 
-	and item_id in (SELECT item_id FROM in_many_shops)
+	and item_id in (SELECT item_id FROM many_offers)
 	ORDER BY item_id ASC, shop_id ASC),
 price_compare AS
 	(SELECT item_id, MAX(price) as highest_price, MIN(price) as lowest_price 
@@ -14,5 +14,5 @@ price_compare AS
 	GROUP BY item_id)
 
 SELECT * from price_compare
-WHERE highest_price > lowest_price*2
+WHERE highest_price >= lowest_price*2
 
