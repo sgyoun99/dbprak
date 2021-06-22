04dbf85f1d3db8412d9cc7930d8375feeb73b148
sgyoun99 <sgyoun99@gmail.com>
2021-06-22 23:05:10 +0200

    edit 4.sql

--- Testat2/4.sql
+++ Testat2/4.sql
@@ -2,4 +2,4 @@ SELECT item_id, MAX(price) as highest_price, MIN(price) as lowest_price
 	FROM item_shop
 	WHERE availability='true'
 	GROUP BY item_id
-	HAVING MAX(price) >= MIN(price)*2
+	HAVING MAX(price) > MIN(price)*2
