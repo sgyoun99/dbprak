a11e257626af6d356949c0480a90437cf4b83555
sgyoun99 <sgyoun99@gmail.com>
2021-06-22 12:26:13 +0200

    Finished 10.sql

--- Testat2/10-4.sql
+++ Testat2/10-4.sql
@@ -1,6 +1,6 @@
 -- shows how different the main categories between item und similar items are.
 CREATE OR REPLACE FUNCTION select_main_cat_diff (param_item_id TEXT, param_sim_item_id TEXT)
-RETURNS TABLE(item TEXT, item_id TEXT, main_cat INTEGER, category_name TEXT, diff TEXT)
+RETURNS TABLE(item TEXT, item_id TEXT, main_cat TEXT, category_name TEXT, diff TEXT)
 language plpgsql
 AS 
 $$
@@ -11,21 +11,21 @@ RETURN 	QUERY
 	main_categories AS (
 	SELECT 	'item' as item,
 			param_item_id as item_id, 
-			select_all_main_cat_id(param_item_id) as main_cat,
+			CAST(select_all_main_cat_id(param_item_id) AS TEXT) as main_cat,
 			'-' as diff
 	UNION
 	SELECT	'similar_item' as item,
 			similar_item_id as item_id, 
-			main_cat.main_cat_id,
+			CAST(main_cat.main_cat_id AS TEXT),
 			CASE WHEN main_cat.main_cat_id in (SELECT * FROM select_all_main_cat_id(param_item_id)) THEN 'SAME' ELSE 'DIFFERENT' END AS diff
 	FROM similar_items, select_all_main_cat_id(similar_item_id) main_cat
 	WHERE similar_item_id = param_sim_item_id
 	)
 
 	SELECT mc.item, mc.item_id, mc.main_cat, category.name, mc.diff
-	FROM main_categories mc INNER JOIN category ON mc.main_cat = category.category_id
+	FROM main_categories mc INNER JOIN category ON mc.main_cat = CAST(category.category_id AS TEXT)
 	UNION
-	SELECT ' = = = = = ', ' = = = = = ', '0', ' = = = = = ', ' = = = = = '
+	SELECT ' = = = = = = = = = = ', ' = = = = = = = = = = ', ' = = = = = = = = = = ', ' = = = = = = = = = = ', ' = = = = = = = = = = '
 	ORDER BY item ASC, main_cat ASC;
 
 END;
--- Testat2/10-5.sql
+++ Testat2/10-5.sql
@@ -3,8 +3,7 @@
 WITH
 diff_main_category AS (
 	SELECT s.item_id, s.similar_item_id, dmc.diff_main_cat_id
-	FROM similar_items s, 
-		select_sim_item_id_with_diff_main_cat(s.item_id, s.similar_item_id) dmc
-	WHERE s.item_id IN (SELECT item_id FROM similar_items LIMIT 100))  -- because it takes too long...
+	FROM (SELECT * FROM similar_items LIMIT 50) s,  -- because it takes too long...
+		select_sim_item_id_with_diff_main_cat(s.item_id, s.similar_item_id) dmc)
 
 SELECT diff.* FROM diff_main_category dmc, select_main_cat_diff(dmc.item_id, dmc.similar_item_id) diff
