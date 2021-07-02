bfdab897e0256671b65b824f23ff55674516dd45
sgyoun99 <sgyoun99@gmail.com>
2021-07-02 23:08:02 +0200

    minor update

--- Testat2/12.sql
+++ Testat2/12.sql
@@ -8,14 +8,12 @@ items_in_all_shop AS (
 cheap_items AS (	
 	SELECT item_shop.item_id, MIN(item_shop.price) as min_price
 	FROM items_in_all_shop iias INNER JOIN item_shop ON iias.item_id = item_shop.item_id 
-								INNER JOIN shop ON item_shop.shop_id = shop.shop_id
-	WHERE iias.item_id = item_shop.item_id
 	GROUP BY item_shop.item_id),
 cheap_items_in_leipzig AS (
 	SELECT * 
 	FROM cheap_items ci INNER JOIN item_shop ON (ci.item_id = item_shop.item_id AND ci.min_price = item_shop.price)
 	WHERE item_shop.shop_id = (SELECT shop_id FROM shop WHERE shop.shop_name = 'Leipzig'))
 
-SELECT ((SELECT COUNT(*) * 100 FROM cheap_items_in_leipzig) / (SELECT COUNT(*) FROM cheap_items)::FLOAT)::NUMERIC(10,2)
+SELECT ((SELECT COUNT(*) * 100 FROM cheap_items_in_leipzig) / (SELECT COUNT(*) FROM items_in_all_shop)::FLOAT)::NUMERIC(10,2)
 --SELECT COUNT(*) FROM cheap_items_in_leipzig --248
---SELECT COUNT(*) FROM cheap_items --307
+--SELECT COUNT(*) FROM items_in_all_shop --307
